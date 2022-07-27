e2a18d98ddcbcee0329af7326073102bdf032c52
Mahmud Jamal <mahmudjamal014@gmail.com>
2022-07-26 20:58:48 -0600

    Grid html elements created

---
+++ app.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        const col = document.createElement('div');
+        col.classList.add('col');
+        row.appendChild(col);
+    }
+    document.querySelector('.container').appendChild(row);
+}
