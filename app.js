f4e5d68d1618c758ac3b77765e838d6f031bee9f
Mahmud Jamal <mahmudjamal014@gmail.com>
2022-07-27 19:31:30 -0600

    Aligned grid and button to center, changed css; project complete

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const grid = document.querySelector('.grid');
 
 function renderGrid(numSquares) {
-    
     grid.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${numSquares}, 1fr)`;
     
     for (let i = 0; i < numSquares * numSquares; i++) {
         const square = document.createElement('div');
-        square.style.border = '2px solid black';
-        square.style.margin = '-1px';
         square.addEventListener('mouseover', () => {
             if (square.style.backgroundColor == '') {
                 square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
             }
         });
-
+        
         grid.appendChild(square);
     }
 }
